227c40fa0abdd5e4ad70bc9ccdd6dd28128ee865
咩屁啊 <970695436@qq.com>
2020-12-30 09:06:03 +0800

    <Feat>:更新 deploy

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:XcGuaN-miepia/XcGuaN-miepia.github.io.git master
+# git push -f git@github.com:XcGuaN-miepia/XcGuaN-miepia.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:XcGuaN-miepia/docs.git master:gh-pages
+git push -f git@github.com:XcGuaN-miepia/docs.git master:gh-pages
 
 cd -
